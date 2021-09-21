7e8800e551e804e42c7f43cd8507206c8b549822
Раиль Рафиков <rayraf@LIVE.RU>
2021-09-21 16:03:27 +0500

    Класс перемещен в начало

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-// import { Dxftemplate } from "./dxftemplate";
-
-
-function myFunction() {
-    var type = 'data:application/octet-stream;base64, ';
-    var dxfobj = new Dxftemplate;
-    var text = dxfobj.header + dxfobj.entityexample + dxfobj.footer;
-    var base = btoa(text);
-    var res = type + base;
-    document.getElementById('test').href = res;
-}
-
-myFunction();
-
-// function writeFile(name, value) {
-//     var val = value;
-//     if (value === undefined) {
-//     val = "";
-//     }
-//     var download = document.createElement("a");
-//     download.href = "data:text/plain;content-disposition=attachment;filename=file," + val;
-//     download.download = name;
-//     download.style.display = "none";
-//     download.id = "download"; document.body.appendChild(download);
-//     document.getElementById("download").click();
-//     document.body.removeChild(download);
-// }
-
-// writeFile("9dksk239xwd.dxf", "jxowsjsivneic");
-
-
 class Dxftemplate {
     header = `  0
     SECTION
@@ -17005,3 +16974,35 @@ EOF`
  0.0
    0`
 }
+
+// import { Dxftemplate } from "./dxftemplate";
+
+
+function myFunction() {
+    var type = 'data:application/octet-stream;base64, ';
+    var dxfobj = new Dxftemplate;
+    var text = dxfobj.header + dxfobj.entityexample + dxfobj.footer;
+    var base = btoa(text);
+    var res = type + base;
+    document.getElementById('test').href = res;
+}
+
+myFunction();
+
+// function writeFile(name, value) {
+//     var val = value;
+//     if (value === undefined) {
+//     val = "";
+//     }
+//     var download = document.createElement("a");
+//     download.href = "data:text/plain;content-disposition=attachment;filename=file," + val;
+//     download.download = name;
+//     download.style.display = "none";
+//     download.id = "download"; document.body.appendChild(download);
+//     document.getElementById("download").click();
+//     document.body.removeChild(download);
+// }
+
+// writeFile("9dksk239xwd.dxf", "jxowsjsivneic");
+
+
